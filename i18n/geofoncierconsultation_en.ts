efe6a5cdd8764e7d4d3fb4f281399b727fa6cbe6
Gustry <gustrimaille@yahoo.fr>
2013-12-09 21:19:50 +0100

    i18n, fix #10

---
+++ i18n/geofoncierconsultation_en.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en">
+<context>
+    <name>GeoFoncierConsultation</name>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="181"/>
+        <source>Client</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="182"/>
+        <source>Mot de passe</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="183"/>
+        <source>Territoire</source>
+        <translation>Territory</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="184"/>
+        <source>Lister les dossiers</source>
+        <translation>Get records</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="185"/>
+        <source>Aide</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="191"/>
+        <source>Dossiers</source>
+        <translation>Records</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="188"/>
+        <source>Couche cartographique</source>
+        <translation>Cartographic layer</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="189"/>
+        <source>Enregistrer les dossiers en</source>
+        <translation>Save records</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="190"/>
+        <source>kml</source>
+        <translation>kml</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="197"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="198"/>
+        <source>TÃ©lÃ©charger KML</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="199"/>
+        <source>Archive ZIP</source>
+        <translation>Zip archive</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="200"/>
+        <source>DÃ©tails</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="180"/>
+        <source>Mes dossiers GÃ©oFoncier</source>
+        <translation>My GéoFoncier&apos;s record</translation>
+    </message>
+    <message>
+        <location filename="ui_geofoncierconsultation.py" line="192"/>
+        <source>Liste des documents Ã  tÃ©lÃ©charger :</source>
+        <translation>Documents we can download :</translation>
+    </message>
+</context>
+<context>
+    <name>GeoFoncierConsultationDialog</name>
+    <message>
+        <location filename="geofoncierconsultation.py" line="92"/>
+        <source>Plugin QGIS pour la consultation des dossiers GÃ©oFoncier&lt;br /&gt;&lt;br /&gt;Auteur: Etienne Trimaille&lt;br /&gt;Mail: &lt;a href=&quot;mailto:etienne@trimaille.eu&quot;&gt;etienne@trimaille.eu&lt;/a&gt;
+&lt;br /&gt;&lt;strong&gt;Ce plugin est expÃ©rimental !&lt;/strong&gt;&lt;br /&gt;&lt;br /&gt;Source cartographique : les contributeurs d&apos;&lt;a href=&apos;http://www.openstreetmap.org&apos;&gt;OpenStreetMap&lt;/a&gt;&lt;br/&gt;Fonctionne avec l&apos;API GÃ©oFoncier version 1.2c&lt;br /&gt;Licence : A DEFINIR</source>
+        <translation>Plugin to consult GéoFoncier&apos;s records&lt;br /&gt;&lt;br /&gt;Author : Etienne Trimaille&lt;br /&gt;Mail:&lt;a href=&quot;mailto:etienne@trimaille.eu&quot;&gt;etienne@trimaille.eu&lt;/a&gt;
+&lt;br /&gt;&lt;strong&gt;This plugin is experimental !&lt;/strong&gt;&lt;br /&gt;&lt;br /&gt;Cartographic layer : &lt;a href=&apos;http://www.openstreetmap.org&apos;&gt;OpenStreetMap&lt;/a&gt;contributors&lt;br/&gt;Works with GéoFoncier&apos;s API version 1.2c&lt;br /&gt;Licence : to def</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="138"/>
+        <source>Mauvais nom d&apos;utilisateur ou mot de passe</source>
+        <translation>Wrong login or password</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="141"/>
+        <source>Aucun dossier pour ce territoire</source>
+        <translation>No records for this territory</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="144"/>
+        <source>Erreur de connexion réseau à GéoFoncier</source>
+        <translation>Network error</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="151"/>
+        <source> dossier trouvé</source>
+        <translation> record</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="153"/>
+        <source> dossiers trouvés</source>
+        <translation> records</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="186"/>
+        <source>Dossier GéoFoncier</source>
+        <translation>GéoFoncier&apos;s records</translation>
+    </message>
+    <message>
+        <location filename="geofoncierconsultation.py" line="302"/>
+        <source>BientÃ´t disponible, en cours de dev</source>
+        <translation>Avialable asap</translation>
+    </message>
+</context>
+</TS>
