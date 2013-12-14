38b3a79693d5000ad7b214e524ae69e5d4326593
Gustry <gustrimaille@yahoo.fr>
2013-12-14 13:38:06 +0100

    Commentaire Saver.py

--- Saver.py
+++ Saver.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*- 
 from PyQt4.QtCore import QUrl, QFileInfo, QFile, QIODevice
-from PyQt4.QtGui import QApplication, QFileDialog, QDialog, QProgressBar, QLabel, QPushButton, QDialogButtonBox, \
-                    QVBoxLayout, QMessageBox, QDesktopServices
+from PyQt4.QtGui import QFileDialog, QDialog, QProgressBar, QLabel, QPushButton, QDialogButtonBox, QVBoxLayout, QMessageBox, QDesktopServices
 from PyQt4.QtNetwork import QHttp
 from urlparse import urlparse, parse_qs
 
-
 class Saver(QDialog):
+    '''Classe qui permet d'enregistrer un fichier sur le disque'''
+    
     def __init__(self, login, password, url, nameFile, parent = None):
+        '''
+        Constructeur
+        @type login: String
+        @param login: Compte utilisateur
+        @type password: String
+        @param password: Mot de passe utilisateur
+        @type url:String
+        @param url: URL de la ressource
+        @type nameFile: String
+        @param nameFile: Nom du fichier
+        '''
+        
         super(Saver, self).__init__(parent)
         
         self.url_to_download = url
@@ -15,7 +27,7 @@ class Saver(QDialog):
         self.password = password
         self.nameFile = nameFile
         
-        urlparse(self.url_to_download).query
+        #urlparse(self.url_to_download).query
         self.parameters = parse_qs(urlparse(url).query)
         
         self.httpGetId = 0
@@ -25,15 +37,17 @@ class Saver(QDialog):
         self.closeButton.setAutoDefault(False)
         self.openFileButton = QPushButton("Ouvrir le fichier")
         self.openFileButton.setAutoDefault(False)
-        self.openDirectoryButton = QPushButton("Ouvrir le repertoire")
+        self.openDirectoryButton = QPushButton(u"Ouvrir le répertoire")
         self.openDirectoryButton.setAutoDefault(False)
         self.progressBar = QProgressBar()
 
+        #Gestion des boutons inférieurs
         buttonBox = QDialogButtonBox()
         buttonBox.addButton(self.openFileButton, QDialogButtonBox.ActionRole)
         buttonBox.addButton(self.openDirectoryButton, QDialogButtonBox.ActionRole)
         buttonBox.addButton(self.closeButton, QDialogButtonBox.RejectRole)
 
+        #Construction de l'objet QHTTP et mise en place des signaux/slots
         self.http = QHttp(self)
         self.http.requestFinished.connect(self.httpRequestFinished)
         self.http.dataReadProgress.connect(self.updateDataReadProgress)
@@ -42,16 +56,20 @@ class Saver(QDialog):
         self.openDirectoryButton.clicked.connect(self.openDirectory)
         self.openFileButton.clicked.connect(self.openFile)
 
+        #Construction de l'interface
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.statusLabel)
         mainLayout.addWidget(self.progressBar)
         mainLayout.addWidget(buttonBox)
         self.setLayout(mainLayout)
 
+        #Création de la fenêtre et affichage
         self.setWindowTitle(u'Enregistrement')
         self.openFileButton.hide()
         self.openDirectoryButton.hide()
         self.show()
+        
+        #Lancement du téléchargement
         self.downloadFile()
 
 
@@ -65,8 +83,7 @@ class Saver(QDialog):
 
         self.outFile = QFile(fileName)
         if not self.outFile.open(QIODevice.WriteOnly):
-            QMessageBox.information(self, 'Error',
-                    'Unable to save the file %s: %s.' % (fileName, self.outFile.errorString()))
+            QMessageBox.information(self, 'Erreur','Impossible d\'enregistrer le fichier %s: %s.' % (fileName, self.outFile.errorString()))
             self.outFile = None
             return
 
@@ -82,7 +99,7 @@ class Saver(QDialog):
         else:
             path = '/'
 
-        #Hack parameters
+        #Concaténation des paramètres à l'URL
         path = path+("?")
         for item in url.queryItems():
             path = path + item[0] + "=" + item[1] + "&" 
@@ -91,7 +108,7 @@ class Saver(QDialog):
         self.httpGetId = self.http.get(path, self.outFile)
 
     def cancelDownload(self):
-        self.statusLabel.setText(u"Enregistrement annule")
+        self.statusLabel.setText(u"Enregistrement annulé")
         self.httpRequestAborted = True
         self.http.abort()
         self.close()
@@ -115,34 +132,33 @@ class Saver(QDialog):
 
         if error:
             self.outFile.remove()
-            QMessageBox.information(self, 'Error',
-                    'Download failed: %s.' % self.http.errorString())
+            QMessageBox.information(self, 'Erreur','Le téléchargement a échoué : %s.' % self.http.errorString())
 
         self.statusLabel.setText('Enregistrement ok')
         self.openDirectoryButton.show()
         self.openFileButton.show()
-        #self.close()      
 
     def readResponseHeader(self, responseHeader):
-        # Check for genuine error conditions.
+        '''Slot pour analyser les headers HTTP'''
         if responseHeader.statusCode() not in (200, 300, 301, 302, 303, 307):
-            print responseHeader.statusCode()
-            QMessageBox.information(self, 'Error',
-                    'Download failed: %s.' % responseHeader.reasonPhrase())
+            QMessageBox.information(self, 'Error','Download failed: %s.' % responseHeader.reasonPhrase())
             self.httpRequestAborted = True
             self.http.abort()
 
     def updateDataReadProgress(self, bytesRead, totalBytes):
+        '''Slot pour mettre à jour la barre de progression'''
         if self.httpRequestAborted:
             return
         self.progressBar.setMaximum(totalBytes)
         self.progressBar.setValue(bytesRead)
         
     def openFile(self):
+        '''Fonction permettant d'ouvrir le fichier'''
         desktopService = QDesktopServices()
         desktopService.openUrl(QUrl(self.outFile.fileName()))
         
     def openDirectory(self):
+        '''Fonction permettant d'ouvrir le répertoire'''
         desktopService = QDesktopServices()
         fileInfo = QFileInfo(QFile(self.outFile.fileName()))
         directory = fileInfo.dir()
